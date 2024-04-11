7c0a64b4ea5389dc3a8a476882499d7add3bfdd4
SwiftSwede <swift-swede@hotmail.com>
2024-04-10 23:23:44 -0500

    Created mouse over effect using JS

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 "use strict";
 
 const container = document.querySelector(".container");
-
+let div;
 for (let i = 0; i < 256; i++) {
-  const div = document.createElement("div");
+  div = document.createElement("div");
   div.classList.add("square");
   container.appendChild(div);
+}
+
+let square = document.querySelectorAll(".square");
+// console.log(square);
+div = document.querySelector("div");
+
+for (let i = 0; i < square.length; i++) {
+  square[i].addEventListener("mouseover", (e) => {
+    square[i].style.backgroundColor = "black";
+  });
 }
